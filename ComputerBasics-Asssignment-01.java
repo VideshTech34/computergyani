07cfebc47422eb82557d5ce717d67b6d51e6a0db
VideshTech34 <videsh5655@gmail.com>
2023-01-26 19:46:54 +0530

    first assignment

---
+++ ComputerBasics-Asssignment-01.java
@@ -0,0 +1,40 @@
+//Computer Basic Information
+
+
+1. What is computer?
+Ans-> A computer is an electronic device that stores and processes data, often in binary form, in accordance with commands from a variable programme.
+
+2.What is ram?
+Ans->This means that information is kept in RAM while the computer is
+running, but it is erased when the machine is powered off. The operating system and other files are frequently
+loaded from an HDD or SSD into RAM when the computer is restarted.
+
+3.Where is data stored in a computer?
+Ans-> A hard disk. That helps to store the files and programs in a computer,  internal store of data store in primary memory.
+
+4.what is that input device used to type text and numbers on a document in the computer system?
+
+Ans-> Keyboard is the most common and very popular input device which helps to input data to the computer. The keyboard is used to type text and numbers on a document in the computer
+
+5.what are the output device?
+
+Ans->
+* Monitor
+* Printer
+* Speaker
+* Plotter
+
+6 What is the input device that allows a user to move the cursor or pointer on the screen?
+Ans-> Mouse is the input device that allows a user to move the cursor or pointer on the screen.
+
+7.Which language is directly understood by the computer without a translation program?
+Ans-> Machine Language is directly understood by computer, because it is alreadt written in binary form.
+
+8. What are input devices?
+Ans->
+*Keyboard
+*Mouse
+*Scanner
+*Joystick
+
+Thank You!!
